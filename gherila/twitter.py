e6d7b3bde915866200dfb6707d7ae28fca3665b8
s4nica <s4nica@proton.me>
2026-03-22 23:17:53 +0200

    added get_user_tweet function

--- gherila/twitter.py
+++ gherila/twitter.py
@@ -271,3 +271,157 @@ class Twitter:
       mentions=[m.screen_name for m in legacy.entities.user_mentions],
       media=medias
     )
+
+  async def get_user_tweets(self: "Twitter", username: str):
+    """
+    Get 20 tweets of an user account.
+
+    Parameters
+    ----------
+    username : :class:`str`
+      The twitter account username.
+
+    Returns
+    -------
+    :class:`List[TwitterTweet]`
+      A TwitterTweet list containing the data.
+    """
+    user = await self.get_user(username)
+    data = await self.session.request(
+      "GET",
+      f"https://x.com/i/api/graphql/O0epvwaQPUx-bT9YlqlL6w/UserTweets",
+      headers=self.headers,
+      params={
+        "variables": dumps(
+          {
+            "userId": user.id,
+            "count": 20,
+            "includePromotedContent": True,
+            "withQuickPromoteEligibilityTweetFields": True,
+            "withVoice": True,
+          }
+        ),
+        "features": dumps(
+          {
+            "rweb_video_screen_enabled": False,
+            "profile_label_improvements_pcf_label_in_post_enabled": True,
+            "responsive_web_profile_redirect_enabled": False,
+            "rweb_tipjar_consumption_enabled": False,
+            "verified_phone_label_enabled": False,
+            "creator_subscriptions_tweet_preview_api_enabled": True,
+            "responsive_web_graphql_timeline_navigation_enabled": True,
+            "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
+            "premium_content_api_read_enabled": False,
+            "communities_web_enable_tweet_community_results_fetch": True,
+            "c9s_tweet_anatomy_moderator_badge_enabled": True,
+            "responsive_web_grok_analyze_button_fetch_trends_enabled": False,
+            "responsive_web_grok_analyze_post_followups_enabled": True,
+            "responsive_web_jetfuel_frame": True,
+            "responsive_web_grok_share_attachment_enabled": True,
+            "responsive_web_grok_annotations_enabled": True,
+            "articles_preview_enabled": True,
+            "responsive_web_edit_tweet_api_enabled": True,
+            "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
+            "view_counts_everywhere_api_enabled": True,
+            "longform_notetweets_consumption_enabled": True,
+            "responsive_web_twitter_article_tweet_consumption_enabled": True,
+            "tweet_awards_web_tipping_enabled": False,
+            "content_disclosure_indicator_enabled": True,
+            "content_disclosure_ai_generated_indicator_enabled": True,
+            "responsive_web_grok_show_grok_translated_post": False,
+            "responsive_web_grok_analysis_button_from_backend": True,
+            "post_ctas_fetch_enabled": True,
+            "freedom_of_speech_not_reach_fetch_enabled": True,
+            "standardized_nudges_misinfo": True,
+            "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": True,
+            "longform_notetweets_rich_text_read_enabled": True,
+            "longform_notetweets_inline_media_enabled": False,
+            "responsive_web_grok_image_annotation_enabled": True,
+            "responsive_web_grok_imagine_annotation_enabled": True,
+            "responsive_web_grok_community_note_auto_translation_is_enabled": False,
+            "responsive_web_enhance_cards_enabled": False
+          }
+        ),
+        "fieldToggles": dumps(
+          {
+            "withArticlePlainText": False
+          }
+        )
+      }
+    )
+
+    entries = next(
+      (
+        instruction.entries
+        for instruction in data.data.user.result.timeline.timeline.instructions
+        if instruction.type == "TimelineAddEntries"
+      ),
+      None
+    )
+    tweets = []
+
+    for entry in entries:
+      if entry.entryId.startswith("tweet-"):
+        result = entry.content.itemContent.tweet_results.result
+        if (
+          legacy := result.legacy
+        ):
+          text = normalize(
+            "NFKC",
+            (
+              result.note_tweet.note_tweet_results.result.text
+              if "note_tweet" in result
+              else legacy.full_text
+            )
+          )
+          quote_url = (
+            legacy.quoted_status_permalink.expanded
+            if "quoted_status_permalink" in legacy 
+            else None
+          )
+
+          medias = []
+          if "extended_entities" in legacy:
+            for m in legacy.extended_entities.media:
+              media = TwitterMedia(type=m.type)
+
+              if "video_info" in m:
+                mp4 = [
+                  v
+                  for v in m.video_info.variants
+                  if getattr(v, "content_type", "") == "video/mp4"
+                ]
+                if mp4:
+                  media.video_url = sorted(
+                    mp4,
+                    key=lambda x: x.bitrate,
+                  )[-1].url
+
+              if m.type == "photo":
+                media.image_url = m.media_url_https
+
+              medias.append(media)
+
+          tweets.append(
+          TwitterTweet(
+            id=legacy.id_str,
+            text=text,
+            author=user,
+            lang=legacy.lang,
+            likes=legacy.favorite_count,
+            replies=legacy.reply_count,
+            retweets=legacy.retweet_count,
+            quote=legacy.quote_count,
+            retweeted=legacy.retweeted,
+            created_at=legacy.created_at,
+            views=result.views.count if "views" in result else "0",
+            bookmarks=legacy.bookmark_count,
+            is_quote=legacy.is_quote_status,
+            quote_url=quote_url,
+            hashtags=[h.text for h in legacy.entities.hashtags] if "hashtags" in legacy.entities else [],
+            mentions=[m.screen_name for m in legacy.entities.user_mentions] if "user_mentions" in legacy.entities else [],
+            media=medias
+          )
+        )
+
+    return tweets
